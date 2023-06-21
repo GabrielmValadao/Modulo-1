8817c81e325c737d2355f2a8d839c2aba81e5382
Gabriel Valadão <gabriel_valadao@estudante.sesisenai.org.br>
2023-06-21 20:24:17 -0300

    verbalização do js

--- semana 4 19-06 a 23-06/Aulas js/aula-2/script.js
+++ semana 4 19-06 a 23-06/Aulas js/aula-2/script.js
@@ -2,14 +2,49 @@ function clicarNoBotao () {
     var email = document.getElementById("campo-email").value 
     var senha = document.getElementById("campo-senha").value
 
+    document.getElementById("campo-email").classList.remove("input-error")
+    document.getElementById("campo-senha").classList.remove("input-error")
+
     console.log(email)
     console.log(senha)
 
     if (email === "") {
-        alert("E-mail é obrigatório")
+        /* mudando estilo do input após não colocar um informação */
+        /*adiciona um estilo inline
+        document.getElementById('campo-email').style = "border-color: red"
+        alert("E-mail é obrigatório") */
+
+        /*adiciona um estilo direto no ob 
+        document.getElementById("campo-email").style.backgroundColor = 'red'
+        */
+
+        /* estlio recomendado de se usar, linkando css com js */
+        document.getElementById("campo-email").classList.add("input-error")
+        document.getElementById("campo-email").focus()
     }
 
-    if (senha === "") {
-        alert("Senha é obrigatória")
+    else if (senha === "") {
+        document.getElementById("campo-senha").classList.add("input-error")
+        document.getElementById("campo-senha").focus() 
     }
+
+    else {
+        /*codigo de feedback visual ao clicar no button */
+        document.getElementById("login-button").disabled = true
+        document.getElementById("login-button").style.opacity = 0.5
+        /*valor ideial para opacidade é o 0.5 */
+        document.getElementById("login-button").innerText = "Entrando..."
+
+        /*redireciona para outra pagina de minha aplicação*/
+
+        window.location.href = "https://www.google.com.br/?hl=pt-BR&safe=active&ssui=on" 
+    }
+
+    
+
+
+
+
 }
+
+
