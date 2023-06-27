ee9d5b8e2310169006e4ce220c7cfffaedee9bea
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-06-27 19:46:54 -0300

    criação arquivos aula 2

---
+++ semana 5 26-06 a 30-06/funções/aula 2/scritp-aula-2.js
@@ -0,0 +1,13 @@
+const texto = "casa";
+
+console.log(texto);
+
+//indice: index, idx, i
+// inicializa um vetor
+const vetor = ["arroz", "feijao"];
+
+console.log(vetor);
+vetor.push("batata");
+console.log(vetor);
+vetor [1] = "cenoura";
+// dentro dos [ ] será definido o indice que será alterado 
