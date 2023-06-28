b072414e69c8013ef8e1f95b097dcb8448eb4982
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-06-28 12:45:23 -0300

    atualizações script - exemplos de escopo, rest e spread

--- semana 5 26-06 a 30-06/funções/aula 2/scritp-aula-2.js
+++ semana 5 26-06 a 30-06/funções/aula 2/scritp-aula-2.js
@@ -1,9 +1,23 @@
+// "USE STRICT"
+"use strict";
+//scripts de tipo modulo, possuem automaticamente
+// escopo USE STRICT
+// <script scr="script-aula-2.js" type="module></script>"
+// sem a necessidade de declarar no inicio do arquivo 
 
+//use sctrict impede que variaveis não explicitamente 
+// definidas sejam usadas 
+// hahahahaDeRaioLaser = "batata"
+// console.log(hahahahDeRaioLaser);
 
+// VARIAVEL SIMPLES
 const texto = "casa";
 
 console.log(texto);
 
+//VARIAVEIS COMPOSTAS
+
+// VETORES / ARRAYS
 //indice: index, idx, i
 // inicializa um vetor
 const vetor = ["arroz", "laranja", "feijao"];
@@ -54,3 +68,51 @@ const listaPessoas = [
 ];
 // acessa a profissão da segunda pessoa
 console.log(listaPessoas[1].profissao);
+
+//ESCOPO
+// escopo de funcao
+// var obedece apensar escopo de funcao
+function definirLargura (){
+    var largura = 100;
+    console.log(definirLargura);
+}
+//definirLargura();
+//console.log(largura);
+
+// escopo de bloco
+// var não é contida em escopo de bloco
+if (true) {
+    var lagura = 999;
+    console.log(largura);
+}
+console.log(largura);
+
+//const e let são contidas em escopo de bloco
+// e tambem escopo de função
+if (true) {
+    const comprimento = 111;
+    console.log(comprimento);
+}
+//erro pois comprimento nao existe fora do bloco
+console.log(comprimento);
+
+//HOSTING/IÇAMENTO
+// "sobe" a definição de funcoes e vars
+// para poder utilizar antes da definição
+teste();
+function teste() {
+    console.log("TESTE_EXECUTOU");
+}
+
+//REST 
+function calcula(a, b, ...batata){
+    console.log(a, b, ...batata);
+}
+calcula(1, 3, 8, 2, 6);
+
+//SPREAD 
+const exemploSpread = [4, 5, 6, 7, 9];
+
+calcula(...exemploSpread);
+//equivalente à 
+calcula(4, 5, 6, 7, 9);
