2883f25f67aa34c7220c8205455e7fece787f03a
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-07-04 13:06:09 -0300

    exemplo-find finalizado

--- semana 6 03-07 a 07-07/aula 1/exemplo-find.js
+++ semana 6 03-07 a 07-07/aula 1/exemplo-find.js
@@ -1,7 +1,10 @@
 const nomes = [
-    {nome: jose, cpf:23456787636},
-    {nome: joana, cpf:23456787633},
-    {nome: guilherme, cpf:23456787632}
+    {nome: 'jose', cpf: '23456787636'},
+    {nome: 'joana', cpf: '23456787633'},
+    {nome: 'guilherme', cpf: '23456787632'},
 ]
 
+//usamos a const para poder utilizar o if
+const usuario = nomes.find((item) => item.nome.includes('joana'))
+                 // vai retornar o primeiro dado encontrado
 
