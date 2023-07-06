b4eecf1bde6cb17d8881b39fddce7af7ff9b586f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-07-05 22:24:26 -0300

    alterações após a aula de 05/07

--- semana 4 19-06 a 23-06/Aulas js/aula-2/scripts/seguidores.js
+++ semana 4 19-06 a 23-06/Aulas js/aula-2/scripts/seguidores.js
@@ -1,80 +1,30 @@
-const seguidores = [
-  {
-    url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'João Silva',
-        quantidade_seguidores: 1000,
-        quantidade_publicacoes: 50
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Maria Santos',
-        quantidade_seguidores: 500,
-        quantidade_publicacoes: 120
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Pedro Oliveira',
-        quantidade_seguidores: 200,
-        quantidade_publicacoes: 75
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Ana Souza',
-        quantidade_seguidores: 800,
-        quantidade_publicacoes: 100
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Lucas Pereira',
-        quantidade_seguidores: 350,
-        quantidade_publicacoes: 200
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Camila Alves',
-        quantidade_seguidores: 1500,
-        quantidade_publicacoes: 90
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'João Silva',
-        quantidade_seguidores: 1000,
-        quantidade_publicacoes: 50
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Maria Santos',
-        quantidade_seguidores: 500,
-        quantidade_publicacoes: 120
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Pedro Oliveira',
-        quantidade_seguidores: 200,
-        quantidade_publicacoes: 75
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Ana Souza',
-        quantidade_seguidores: 800,
-        quantidade_publicacoes: 100
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Lucas Pereira',
-        quantidade_seguidores: 350,
-        quantidade_publicacoes: 200
-    },
-    {
-        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
-        nome: 'Camila Alves',
-        quantidade_seguidores: 1500,
-        quantidade_publicacoes: 90
-  },
-];
+import {seguidores} from '../constantes/seguidores.js'
+
+
+document.getElementById('form-pesquisa').addEventListener('submit', (event) => {
+    event.preventDefault() //cancela o reload ao clicar no enter
+    
+    const valorDigitadoPesquisa = document.getElementById('input-pesquisa').value
+
+    const seguidoresFiltrados = seguidores.filter((seguidor) => seguidor.nome === valorDigitadoPesquisa)
+
+    document.getElementById('corpo-tabela').innerHTML = ''
+
+    //continua amanhã
+})
 
 function criarLinhasTabela () {
-    seguidores.map((seguidor) => { 
+
+    const seguidoresFormatados = seguidores.map(seguidor => {
+        return {
+            ...seguidor,
+            apelido: "#" + seguidor.nome.toLowerCase().replace(" ", "_")
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, '')
+        }
+    })
+
+    seguidoresFormatados.map((seguidor) => { 
     //inicio do loop
 
     //criar uma linha do html de cada vez
