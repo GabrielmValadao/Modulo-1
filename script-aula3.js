70179a4fa42851f224366ed4d7bb3156b88cc168
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-07-12 19:37:45 -0300

    criação dos itens da lista atraves do js

--- script-aula3.js
+++ script-aula3.js
@@ -1,1 +1,29 @@
 console.log("m1_s07_a3");
+
+const elemLista = document.getElementById("lista-itens");
+
+const dadosLista = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+];
+
+function criaItem (item) {
+    const li = document.createElement("li");
+    li.innerHTML = item;
+    return li;
+}
+
+function atualizaTela() {
+    elemLista.innerHTML = "";
+
+    dadosLista.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerHTML = item;
+
+        elemLista.appendChild(li);
+    })
+}
+
+
+atualizaTela();
