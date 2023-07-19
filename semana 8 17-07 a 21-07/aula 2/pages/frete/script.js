38cd8ac169a0911b75178a2eaab3fdd3e80c41a3
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-07-18 21:48:46 -0300

    exemplo cep finalizado

--- semana 8 17-07 a 21-07/aula 2/pages/frete/script.js
+++ semana 8 17-07 a 21-07/aula 2/pages/frete/script.js
@@ -5,23 +5,26 @@ function exibirDados(event) {
 
     const cep = document.getElementById('cep').value;
 
-    if (cep.length > 8 )    
-    fetch(`https://viacep.com.br/ws/${cep}/json/`) 
+    if (cep.length === 8 || cep.length === 9 ) {    
+    fetch(`https://viacep.com.br/ws/${cep}/json/`) //GET 
     // captura a resposta 
     .then ((response) => response.json())
     .then ((data) => {
         const resultDiv = document.getElementById('result');
-        
-            resultDiv.innerHTML =
+            resultDiv.innerHTML = `
             <p>Endereco: ${data.logradouro}</p>
             <p>Bairro: ${data.bairro}</p>
             <p>Cidade: ${data.localidade}</p>
             <p>UF: ${data.uf}</p>
             <p>DDD: ${data.ddd}</p>
+            `
     })
     // pega um erro que acaba acontecendo dentro de then
     .catch(() => {
-        alert("Erro ao fazer a solicitação");
+        alert("Erro ao fazer a solicitação")
     })
+} else {
+    alert('Digite o cep completo')
 }
 
+}
