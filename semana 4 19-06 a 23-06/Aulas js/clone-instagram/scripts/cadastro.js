6c0897fa4bfa84686b8d87ec015b24e09a2d7df0
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-07-20 22:08:06 -0300

    adiciona json ao projeto

---
+++ semana 4 19-06 a 23-06/Aulas js/clone-instagram/scripts/cadastro.js
@@ -0,0 +1,85 @@
+const regexTelefone = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
+const regexEMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const campoNomeCompleto = document.getElementById('campo-nome-completo')
+const campoSenha = document.getElementById('campo-senha')
+const campoTelefone = document.getElementById('campo-telefone')
+const campoEmail = document.getElementById('campo-email')
+
+document.getElementById('form-cadastro').addEventListener('submit', fazerCadastro)
+
+function fazerCadastro(event) {
+
+    event.preventDefault()
+
+    const nome = campoNomeCompleto.value
+    const senha = campoSenha.value
+    const telefone = campoTelefone.value
+    const email = campoEmail.value
+
+    campoNomeCompleto.classList.remove("input-error")
+    document.getElementById('error-nome').hidden = true
+
+    campoSenha.classList.remove("input-error")
+    document.getElementById('error-senha').hidden = true
+
+    campoTelefone.classList.remove("input-error")
+    document.getElementById('error-telefone').hidden = true
+
+    campoEmail.classList.remove("input-error")
+    document.getElementById('error-email').hidden = true
+
+    if (nome === "") {
+        campoNomeCompleto.classList.add('input-error')
+        campoNomeCompleto.focus()
+        document.getElementById('error-nome').hidden = false
+        document.getElementById('error-nome').innerText = "O nome é obrigatório"
+    } else if (senha.length < 9) {
+        campoSenha.classList.add('input-error')
+        document.getElementById('error-senha').hidden = false
+        document.getElementById('error-senha').innerText = "A senha deve no minimo 8 caracteres"
+    } else if (regexTelefone.test(telefone) === false) {
+        campoTelefone.classList.add('input-error')
+        document.getElementById('error-telefone').hidden = false
+        document.getElementById('error-telefone').innerText = "Digite um telefone válido"
+    } else if (regexEMail.test(email) === false) {
+        campoEmail.classList.add('input-error')
+        document.getElementById('error-email').hidden = false
+        document.getElementById('error-email').innerText = "Digite um email válido"
+    } else {
+        // cadastrar os dados que foram digitados no formulario
+
+        /*      
+            GET - lista varias informações 
+            POST - cadastra uma novo recurso
+            DELETE - deletar um recurso
+            PUT - Atualiza todas as informacoes 
+            PATCH - Atualiza uma informação 
+        */
+
+        document.getElementById('login-button').style.opacity = 0.5
+        document.getElementById('login-button').innerText = "Cadastrando ...."
+        document.getElementById('login-button').disabled = true
+    
+        fetch('http://localhost:3333/usuarios', {
+            method: 'POST',
+            body: JSON.stringify({
+                nome: nome,
+                senha: senha,
+                telefone: telefone,
+                email: email
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(() => {
+            alert('Cadastrado com sucesso')
+            window.location.href = "./index.html"
+        })
+        .catch(() => {
+            alert("Desculpe. Houve um erro ao cadastrar o usuario")
+        })
+    }
+}
--- semana 4 19-06 a 23-06/Aulas js/clone-instagram/utils/regexs.js
+++ semana 4 19-06 a 23-06/Aulas js/clone-instagram/utils/regexs.js
@@ -1,3 +1,4 @@
 // validação dos dados do telefone como padrão no brasil, atraves de rejects, solicitar rejects sobre o que quero validar atraves do chatgpt 
 export const regexTelefone = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
+
 export const regexEMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
