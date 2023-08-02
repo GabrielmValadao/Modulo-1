3bb909ffe0a064214d83f430a92c4829edb75238
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-01 21:37:09 -0300

    cria codigo de calculo de imc

--- semana 9 31-07 a 04-08/aula-02/src/App.vue
+++ semana 9 31-07 a 04-08/aula-02/src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-
+    <h1>Calculadora de IMC</h1>
+    <input type="number" placeholder="Peso" v-model="peso" />
+    <input type="number" placeholder="Altura" v-model="altura" />
+    <button @click="calcular()">Calcular IMC</button>
+    <span>{{ resultado }}</span> 
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      peso: 0,
+      altura: 0,
+      resultado: ""
+    }
+  },
+
+  methods: {
+    calcular() {
+      if(this.peso == 0 || this.altura == 0) {
+        alert("Os dados precisam ser maiores que 0")
+        return
+      }
+
+      this.resultado = (this.peso / (this.altura ** 2)).toFixed(2)
+    }
+  }
   
 }
 </script>
 
 <style>
-
+    
 </style>
