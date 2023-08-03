2aba0188e4e6788594968c74f73870bc94bc0da7
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-03 12:54:49 -0300

    declara a estrutura e props do CursoItem

--- semana 9 31-07 a 04-08/lista-cursos-aula-03/lista-cursos/src/App.vue
+++ semana 9 31-07 a 04-08/lista-cursos-aula-03/lista-cursos/src/App.vue
@@ -3,16 +3,24 @@
   <div>
     <header>
       <h1>Lista de cursos</h1>
+      <!-- 3 - Reendeniza na tela --> 
+      <CursoItem />
     </header>
 
   </div>
 </template>
 
 <script>
+// 1 - importa (sempre fora do export)
+import CursoItem from "./components/CursoItem.vue"
 export default {
+  //2 - registra 
+  components: {
+    CursoItem
+  },
   // cria a lista dos cursos
+  // função que retornar um objeto
   data() {
-    // função que retornar um objeto
     return {
       listaCursos: [
         {
---
+++ semana 9 31-07 a 04-08/lista-cursos-aula-03/lista-cursos/src/components/CursoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Título: {{ titulo }} </h1> <br>
+        <p>Duração: {{ duracao }}</p> <br>
+        <p>Vagas: {{ vagas }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['titulo', 'duracao', 'vagas']
+    } 
+</script>
+
+<style scoped>
+    h1 {
+        color: gray;
+    }
+</style>
