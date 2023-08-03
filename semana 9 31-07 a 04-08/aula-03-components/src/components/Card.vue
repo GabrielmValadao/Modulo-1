0242a9008a2a606b054166cd787519d37947f560
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-02 21:04:56 -0300

    adiciona exemplos de slot name

---
+++ semana 9 31-07 a 04-08/aula-03-components/src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <slot name="header">
+
+        </slot>
+    </header>
+    <slot></slot>
+
+    <footer>
+        <slot name="footer"></slot>
+    </footer>
+    <!-- <h1>Card</h1> -->
+    <!-- expecifica que vamos colocar algum conteudo dentro de card -->
+    <!-- <slot></slot> -->
+</template>
+
+<script>
+</script>
+
+<style scoped></style>
