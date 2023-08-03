65e0cbebb7968621c6f2f3ff6d11537df312b16f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-03 12:30:28 -0300

    cria lista dos cursos

--- semana 9 31-07 a 04-08/lista-cursos-aula-03/lista-cursos/src/App.vue
+++ semana 9 31-07 a 04-08/lista-cursos-aula-03/lista-cursos/src/App.vue
@@ -1,12 +1,39 @@
 
 <template>
   <div>
+    <header>
+      <h1>Lista de cursos</h1>
+    </header>
 
   </div>
 </template>
 
 <script>
-
+export default {
+  // cria a lista dos cursos
+  // função que retornar um objeto
+  data() {
+    return {
+      listaCursos: [
+        {
+          titulo: "HTML e CSS",
+          duracao: 20,
+          vagas: 40
+        },
+        {
+          titulo: "Javascript",
+          duracao: 20,
+          vagas: 40
+        },
+        {
+          titulo: "PHP",
+          duracao: 20,
+          vagas: 40
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style scoped></style>
