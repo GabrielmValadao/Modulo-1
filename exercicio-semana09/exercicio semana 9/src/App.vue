5a872d9b7b0e303e76a23db6cd22c12498c7c0f9
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-06 20:45:23 -0300

    revisa codigo final

--- exercicio-semana09/exercicio semana 9/src/App.vue
+++ exercicio-semana09/exercicio semana 9/src/App.vue
@@ -1,61 +1,67 @@
 <template>
   <Header />
   <FormularioNovoMedicamento @cadastrar="AdicionaMedicamento" />
-  <CardMedicamento 
-  v-if="!!listaMedicamentos"
-  v-for="medicamento in listaMedicamentos"
-  :key="medicamento.id"
-  @favoritar ="FavoritarMedicamento" :nome="medicamento.nome" :laboratorio="medicamento.laboratorio" :preco="medicamento.preco" :id="medicamento.id"/>
   <div class="container">
-
+    <CardMedicamento v-if="!!listaMedicamentos" v-for="medicamento in listaMedicamentos" :key="medicamento.id"
+    @favoritar="FavoritarMedicamento" 
+    :nome="medicamento.nome" 
+    :laboratorio="medicamento.laboratorio"
+    :preco="medicamento.preco" 
+    :id="medicamento.id" 
+    />
   </div>
 </template>
+    
 <script>
-    import {v4 as uuidv4} from 'uuid'; 
-    import Header from "./components/Header.vue"
-    import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento/index.vue"
-    import CardMedicamento from "./components/CardMedicamento/index.vue"
+import { v4 as uuidv4 } from 'uuid';
+import Header from "./components/Header.vue"
+import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento/index.vue"
+import CardMedicamento from "./components/CardMedicamento/index.vue"
 
-  export default {
-    components : {
-      Header,
-      FormularioNovoMedicamento, 
-      CardMedicamento
-    },
-    data() {
-      return {
-        listaMedicamentos: []
+export default {
+  components: {
+    Header,
+    FormularioNovoMedicamento,
+    CardMedicamento
+  },
+  data() {
+    return {
+      listaMedicamentos: []
+    }
+  },
+
+  methods: {
+    AdicionaMedicamento(nome, laboratorio, preco) {
+      if (nome == "" || laboratorio == "" || preco == 0) {
+        alert("Preencha todas as informações necessárias!")
+        return
       }
+
+      const novoMedicamento = {
+        id: uuidv4(),
+        nome: nome,
+        laboratorio: laboratorio,
+        preco: preco,
+        favorito: false
+      }
+      this.listaMedicamentos.push(novoMedicamento)
+
     },
-
-    methods: {
-      AdicionaMedicamento(nome, laboratorio, preco) {
-        if (nome == "" || laboratorio == "" || preco == 0) {
-          alert("Preencha todas as informações necessárias!")
-          return
+    FavoritarMedicamento(id) {
+      //editar o medicamento e marca como favorito 
+      this.listaMedicamentos = this.listaMedicamentos.map(item => {
+        if (item.id == id) {
+          item.favorito = !item.favorito
         }
-
-        const novoMedicamento = {
-          id: uuidv4(), 
-          nome: nome,
-          laboratorio: laboratorio,
-          preco: preco,
-          favorito: false 
-        }  
-          this.listaMedicamentos.push(novoMedicamento)
-        
-        },
-        FavoritarMedicamento(id) {
-          //editar o medicamento e marca como favorito 
-          this.listaMedicamentos = this.listaMedicamentos.map(item => {
-            if(item.id == id){
-              item.favorito = !item.favorito
-            }
-          })
-        } 
+      })
     }
   }
+}
 </script>
 <style scoped>
-  
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 1em;
+  }
 </style>
