37ce298f5e753452ea7e0961e6830dfbc3c60054
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-09 19:44:18 -0300

    ajustes no css

--- semana 10 07-08 a 11-08/aula1/aula1 07.08/src/views/SignUp/SignUp.vue
+++ semana 10 07-08 a 11-08/aula1/aula1 07.08/src/views/SignUp/SignUp.vue
@@ -1,126 +1,208 @@
 <template>
-    <div>
-        <form class="formulario">
-            <h2>Criar Conta</h2>
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" placeholder="Digite seu nome completo" v-model="nome" />
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" placeholder="Digite seu email para cadastro" v-model="email" />
-            <label for="telefone">Telefone:</label>
-            <input type="number" id="telefone" placeholder="Digite seu telefone" v-model="telefone" />
-            <label for="senha">Senha</label>
-            <input type="password" id="senha" placeholder="Digite sua senha" v-model="senha" />
-            <label for="confimar-senha">Confirmar senha:</label>
-            <input type="password" id="confirmar-senha" placeholder="Confirme sua senha" v-model="confirmarSenha" />
+  <div>
+    <form class="formulario">
+      <h2>Criar Conta</h2>
+      <div class="form-element">
+        <label for="nome">Nome:</label>
+        <input
+          class="form-input"
+          type="text"
+          id="nome"
+          placeholder="Digite seu nome completo"
+          v-model="nome"
+        />
+      </div>
+      <div class="form-element">
+        <label for="email">E-mail:</label>
+        <input
+          class="form-input"
+          type="email"
+          id="email"
+          placeholder="Digite seu email para cadastro"
+          v-model="email"
+        />
+      </div>
+      <div class="form-element">
+        <label for="telefone">Telefone:</label>
+        <input
+          class="form-input"
+          type="number"
+          id="telefone"
+          placeholder="Digite seu telefone"
+          v-model="telefone"
+        />
+      </div>
+      <div class="form-element">
+        <label for="senha">Senha</label>
+        <input
+          class="form-input"
+          type="password"
+          id="senha"
+          placeholder="Digite sua senha"
+          v-model="senha"
+        />
+      </div>
+      <div class="form-element">
+        <label for="confimar-senha">Confirmar senha:</label>
+        <input
+          class="form-input"
+          type="password"
+          id="confirmar-senha"
+          placeholder="Confirme sua senha"
+          v-model="confirmarSenha"
+        />
+      </div>
 
+      <div class="form-element">
+        <label for="sponsor">Patrocinador:</label>
+        <select class="form-select" id="sponsor" v-model="sponsor">
+          <option value="">Sem patrocinador</option>
+          <option value="Coca cola">Coca Cola</option>
+          <option value="Lab 365">Lab 365</option>
+          <option value="Zucchetti">Zucchetti</option>
+        </select>
+      </div>
 
+      <div class="form-element">
+        <label for="biografia">Sua biografia:</label>
+        <textarea
+          class="form-textarea"
+          id="biografia"
+          cols="30"
+          rows="10"
+          placeholder="Fale sobre você"
+          v-model="bio"
+        >
+        </textarea>
+      </div>
 
-            <label for="sponsor">Patrocinador:</label>
-            <select id="sponsor" v-model="sponsor">
-                <option value="">Sem patrocinador</option>
-                <option value="Coca cola">Coca Cola</option>
-                <option value="Lab 365">Lab 365</option>
-                <option value="Zucchetti">Zucchetti</option>
-            </select>
-
-            <label for="biografia">Sua biografia:</label>
-            <textarea id="biografia" cols="30" rows="10" placeholder="Fale sobre você" v-model="bio">
-            </textarea>
-
-            <label for="confirmar-termos">
-                <input type="checkbox" id="confirmar-termos" v-model="confirmarTermos"> Aceita termos de uso
-            </label>
-
-            <input type="radio" value="1" v-model="tipoPlano" /> <label for="tipo-plano">Bronze</label>
-            <input type="radio" value="2" v-model="tipoPlano" /> <label for="tipo-plano">Prata</label>
-            <input type="radio" value="3" v-model="tipoPlano" /> <label for="tipo-plano">Premium</label>
-
-        </form>
-    </div>
+      <div class="form-element">
+          <p>Selecione um tipo de plano:</p>
+          <div class="form-radio">
+              <input type="radio" value="1" v-model="tipoPlano" />
+              <label for="tipo-plano">Bronze</label>
+            </div>
+            <div class="form-radio">
+                <input type="radio" value="2" v-model="tipoPlano" />
+                <label for="tipo-plano">Prata</label>
+            </div>
+            <div class="form-radio">
+                <input type="radio" value="3" v-model="tipoPlano" />
+                <label for="tipo-plano">Premium</label>
+            </div>
+        </div>
+        <label for="confirmar-termos">
+          <input
+            type="checkbox"
+            id="confirmar-termos"
+            v-model="confirmarTermos"
+          />
+          Aceita termos de uso
+        </label>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            nome: '',
-            email: '',
-            telefone: '',
-            senha: '',
-            confirmarSenha: '',
-            sponsor: '',
-            bio: '',
-            confirmarTermos: true,
-            tipoPlano: '1'
-        };
-    },
+  data() {
+    return {
+      nome: "",
+      email: "",
+      telefone: "",
+      senha: "",
+      confirmarSenha: "",
+      sponsor: "",
+      bio: "",
+      confirmarTermos: true,
+      tipoPlano: "1",
+    };
+  },
 };
 </script>
 
 <style scoped>
 .error-box {
-    background-color: tomato;
-    width: 80%;
-    color: #fff;
+  background-color: tomato;
+  width: 80%;
+  color: #fff;
 }
 
 .formulario {
-    padding: 5px;
-    margin: 40px auto;
-    width: 40%;
+  padding: 5px;
+  margin: 40px auto;
+  width: 40%;
 
-    border-radius: 4px;
-    border: 1px solid #383737;
+  border-radius: 4px;
+  border: 1px solid #383737;
 
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
 
-    padding: 10px;
+  padding: 10px;
 }
 
 .input-area {
-    width: 80%;
+  width: 100%;
 
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .input-area input {
-    width: 100%;
+  width: 100%;
 }
 
 .texto-erro {
-    color: red;
-    margin: 4px;
+  color: red;
+  margin: 4px;
 }
 
-input {
-    padding: 8px;
-    height: 54px;
-    width: 80%;
-    border-radius: 8px;
-    border: 1px solid #756767;
-    outline: none;
+.form-element {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+}
+
+.form-select,
+.form-input,
+.form-textarea {
+  padding: 4px;
+  height: 34px;
+  width: 100%;
+  border-radius: 8px;
+  border: 1px solid #756767;
+  outline: none;
+}
+
+.form-textarea {
+  height: 68px;
+}
+
+.form-radio {
+  display: flex;
+  align-items: center;
+  gap: 5px;
 }
 
 button {
-    width: 80%;
-    height: 54px;
-    background-color: #3578e5;
+  width: 80%;
+  height: 54px;
+  background-color: #3578e5;
 
-    color: white;
-    font-size: 18px;
-    border-radius: 8px;
-    border: none;
+  color: white;
+  font-size: 18px;
+  border-radius: 8px;
+  border: none;
 }
 
 button:hover {
-    background-color: #286ee0;
+  background-color: #286ee0;
 }
 
 .input-error {
-    border-color: red;
+  border-color: red;
 }
 </style>
