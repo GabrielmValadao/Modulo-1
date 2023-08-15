7eb786a9c4fc1fc3395e385a48e14bbf8a6daa40
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-15 20:48:12 -0300

    cria exercicio pratico de ciclo de vida e axios

---
+++ semana 11 14-08 a 18-08/exercicio-pratico/exercicio-pratico/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Informações do endereço</h1>
+    <p> CEP: {{ endereco.cep }}</p>
+    <p> Logradouro: {{ endereco.logradouro }}</p>
+    <p> Complemento: {{ endereco.complemento }}</p>
+    <p> Bairro: {{ endereco.bairro }}</p>
+    <p> Localidade: {{ endereco.localidade }}</p>
+    <p> UF: {{ endereco.uf }}</p>
+    <p> IBGE: {{ endereco.ibge }}</p>
+    <p> Gia: {{ endereco.gia }}</p>
+    <p> DDD: {{ endereco.ddd }}</p>
+    <p> Siafi: {{ endereco.siafi }}</p>
+    
+  </div>
+</template>
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      endereco: {}
+    }
+  },
+  mounted() {
+    axios.get('https://viacep.com.br/ws/01001000/json/')
+    .then(res => this.endereco = res.data)
+  }
+}
+</script>
+<style scoped>
+  
+</style>
