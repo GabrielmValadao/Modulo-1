6f5880808369c2d5ec942f497ed066c9e16d7441
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-15 19:58:33 -0300

    cria arquivo de exemplo de ciclos de vida e axios

---
+++ semana 11 14-08 a 18-08/exemplo-ciclo-vida-axios/exemplo-ciclos-vida-axios/src/App.vue
@@ -0,0 +1,29 @@
+
+<template >
+  <div>
+    <h1>ciclos de vida
+
+    </h1>
+  </div>
+</template>
+<script>
+/**
+ * PASSOS PARA UTILIZAR O AXIOS
+ * 
+ * 1 - [x] instalar o axios
+ *    `npm install axios`
+ * 2 - [x] importar o axios no local que será utilizado
+ *    `import axios from "axios"`
+ * 3 - [] utilizar o axios usando o método (GET, POST, PUT...) desejado
+ */
+import axios from "axios"
+
+export default {
+  mounted() {
+    axios.get()
+  }
+}
+</script>
+<style scoped>
+  
+</style>
