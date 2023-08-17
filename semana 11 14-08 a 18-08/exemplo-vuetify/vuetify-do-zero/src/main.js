d5b1677e71a7726d5d5a9b216ead9fe86d9647fc
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-17 13:01:45 -0300

    importa o codigo base na main.js (repositorio do bruno)

--- semana 11 14-08 a 18-08/exemplo-vuetify/vuetify-do-zero/src/main.js
+++ semana 11 14-08 a 18-08/exemplo-vuetify/vuetify-do-zero/src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp } from 'vue' 
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+// Vuetify 
+import 'vuetify/styles' 
+import '@mdi/font/css/materialdesignicons.css' 
+import { createVuetify } from 'vuetify' 
+import * as components from 'vuetify/components' 
+import * as directives from 'vuetify/directives'
 
-app.use(router)
+const vuetify = createVuetify({ components, directives, icons: { defaultSet: 'mdi' }, })
 
-app.mount('#app')
+createApp(App).use(vuetify).mount('#app')
