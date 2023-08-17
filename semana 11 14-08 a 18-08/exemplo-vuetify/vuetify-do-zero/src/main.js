c7d527dbbab16a2b1a6fe27187d04f428e54092a
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-17 13:25:22 -0300

    atualiza app.vue

--- semana 11 14-08 a 18-08/exemplo-vuetify/vuetify-do-zero/src/main.js
+++ semana 11 14-08 a 18-08/exemplo-vuetify/vuetify-do-zero/src/main.js
@@ -1,13 +1,27 @@
+import './assets/main.css'
+
 import { createApp } from 'vue' 
 import App from './App.vue'
+import router from './router'
 
-// Vuetify 
 import 'vuetify/styles' 
 import '@mdi/font/css/materialdesignicons.css' 
 import { createVuetify } from 'vuetify' 
 import * as components from 'vuetify/components' 
 import * as directives from 'vuetify/directives'
-
+// Vuetify 
 const vuetify = createVuetify({ components, directives, icons: { defaultSet: 'mdi' }, })
 
-createApp(App).use(vuetify).mount('#app')
+
+
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
+
+
+
+
+
